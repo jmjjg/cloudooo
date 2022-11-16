dc7700fd60e402e145e6273585ebfdc1ca38b230
Christian BUFFIN <christian.buffin@adullact-projet.coop>
2022-11-17 00:36:05 +0100

    Revert setup.py to 76b9d5b6cc9573daad5f82e72131c9e3e83d900f

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '2.0.0+ls'
+version = '1.2.6-dev'
 
 def read(name):
     return open(name).read()
@@ -9,19 +9,15 @@ long_description = (read('README.rst') + '\n' + read('CHANGELOG.rst'))
 
 install_requires = [
           # -*- Extra requirements: -*-
-          'argparse==1.2.1',
-          'erp5.util==0.4.70',
-          'lxml==4.6.2',
-          'paste==3.5.0',
-          'pastedeploy==2.1.1',
-          'pastescript[wsgiutils]==3.2.1',
-          'psutil==5.5.1',
-          'pypdf==1.13',
-          'python-magic==0.4.24',
-          'six==1.14.0',
-          'wsgiserver==1.3',
-          'wsgiutils==0.7.2',
-          'zope.interface==5.2.0'
+          'zope.interface',
+          'PasteDeploy',
+          'PasteScript[WSGIUtils]',
+          'pyPdf',
+          'psutil>=3.0.0',
+          'lxml',
+          'python-magic',
+          'argparse',
+          'erp5.util'
           ]
 
 setup(name='cloudooo',
@@ -33,7 +29,6 @@ setup(name='cloudooo',
         "Topic :: System :: Operating System Kernels :: Linux",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
         "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
         "Natural Language :: English",
         "Framework :: Paste"],
       keywords='xmlrpc openoffice wsgi paste python',
